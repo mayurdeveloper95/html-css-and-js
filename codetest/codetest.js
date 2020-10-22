0eac4d11d3e29539a9ccfed582769ce3fde39d5c
unknown <you@example.com>
2020-10-22 14:21:32 +0530

    code test example

---
+++ codetest/codetest.js
@@ -0,0 +1,25 @@
+//create function a volume of a reactangular prism is calculated as like:
+//volume = length * width * height
+//write a function which calculates volumeOfReactPrism
+
+
+function volumeOfReactPrism(l,w,h)
+{
+console.log(`volume of a reactangular prism is:${l*w*h}`);
+}
+volumeOfReactPrism(2,4,5);
+volumeOfReactPrism(7,1,6);
+
+
+
+
+//Area if a circle is calculated as like: area = pie *r*r.write a function which calculates areaOfCircle
+
+function areaOfCircle(r)
+{
+const pi=3.14;
+console.log(`area of circle is:${pi*r*r}`);
+}
+areaOfCircle(8);
+areaOfCircle(2);
+
